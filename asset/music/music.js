d6df0f3e951b61a67138e4831e22bb890f081b21
Sktirlx <139586882+Sktirlx@users.noreply.github.com>
2025-01-21 19:41:31 +0800

    Add files via upload

--- asset/music/music.js
+++ asset/music/music.js
@@ -1,15 +1,6 @@
 const musicPlayer = document.getElementById('musicPlayer');
 const playPauseButton = document.getElementById('playPauseButton');
 
-playPauseButton.addEventListener('click', () => {
-    if (musicPlayer.paused) {
-        musicPlayer.play();
-        playPauseButton.textContent = '暂停';
-    } else {
-        musicPlayer.pause();
-        playPauseButton.textContent = '播放';
-    }
-});
 
 // 在用户点击页面时尝试自动播放音乐
 document.addEventListener('click', () => {
